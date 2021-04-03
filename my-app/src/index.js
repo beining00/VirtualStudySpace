6eee15862ddb65d614ca470ffef17beafbb0bfdd
ArragonP <54829982+ArragonP@users.noreply.github.com>
2021-04-03 14:16:32 +1100

    Implemented a rudimentary timer function, with start, pause and stop functionality

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import Timer from './components/Timer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Timer />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('TimerRoot')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('AppRoot')
+// );
