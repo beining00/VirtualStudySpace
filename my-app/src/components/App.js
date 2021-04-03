38ebe0a6969101dee2f8ecd82cdbd3e38a4290c8
Jerry2108 <80389972+Jerry2108@users.noreply.github.com>
2021-04-03 17:58:15 +1100

    Update App.js

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -4,15 +4,14 @@ import SignIn from './SignIn';
 import TopBar from './TopBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FriendListItem from './FriendListItem'
-
-
+import GoalAndTime from './GoalAndTime';
+import Name from './Name';
 function App() {
-
   return (
     <div className="App">
-      
       <TopBar /> 
-
+         <Name />
+          <GoalAndTime />
       
     </div>
   );
