f3d8ead549d42ed73b492c2096211980feecf0f7
ArragonP <54829982+ArragonP@users.noreply.github.com>
2021-04-03 19:16:40 +1100

    Resolved merge conflicts

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
+import Timer from './components/Timer';
 //import Timer from './components/Timer';
 
 // ReactDOM.render(
@@ -16,4 +17,10 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('AppRoot')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Timer />
+  </React.StrictMode>,  document.getElementById('TimerRoot')
 );
