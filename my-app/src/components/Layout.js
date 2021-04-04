946015f3f2ef62d490a29cf3e9f10df186c59c8a
Rebecca Chen <beining0026@gmail.com.au>
2021-04-04 21:21:01 +1000

    deploy files

--- my-app/src/components/Layout.js
+++ my-app/src/components/Layout.js
@@ -295,7 +295,11 @@ function Layout() {
                    
                         
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <Col md = '8' >
+=======
+                    <Col md = '8'>
+>>>>>>> parent of 511d42b2 (new authentication)
 =======
                     <Col md = '8'>
 >>>>>>> parent of 511d42b2 (new authentication)
